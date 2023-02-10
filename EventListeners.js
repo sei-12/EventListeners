12118a456b8fec0f7f2432c0bf36c8817f41e7bb
sei-12 <110503762+sei-12@users.noreply.github.com>
2023-02-11 04:47:57 +0900

    Add files via upload

---
+++ EventListeners.js
@@ -0,0 +1,35 @@
+class EventListeners{
+    constructor(configs){
+        this.configs = configs
+    }
+
+    #switch(add_or_remove){
+        this.configs.forEach(config => {
+            if(config instanceof EventListenerConfig == false){
+                throw Error("configはEventListenerConfigのインスタンスである必要があります")
+            }
+
+            if(add_or_remove == "add"){
+                config.elm.addEventListener(config.type,config.callback)
+            }else{
+                config.elm.removeEventListener(config.type,config.callback)
+            }
+        });
+    }
+
+    add(){
+        this.#switch("add")
+    }
+
+    remove(){
+        this.#switch("remove")
+    }
+}
+
+class EventListenerConfig{
+    constructor(elm,type,callback){
+        this.elm = elm
+        this.type = type
+        this.callback = callback
+    }
+}
